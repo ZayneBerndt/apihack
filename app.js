ed33562c78a56db9fe58a73e597e200f226a20e9
Zayne  Berndt <berndt.zayne@gmail.com>
2019-06-22 15:30:00 +0100

    add express and mongoose

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 var express = require('express'),
-  app = express(),
-  port = process.env.PORT || 3000;
+app = express(),
+port = process.env.PORT || 3000;
+mogoose = require('mongoose'), 
+Task = require('./api/models/todoListModel'), //created model loading here
+bodyParser = require('body-parser');
+
+//mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connection('mongodb://localhost/Tododb');
+
+app.use(bodyParser.urlencoded({ extend:true }));
+app.use(bodyParser.json());
+
+
+var routes = require('./api/routes/todoListRoutes'); //importing route
+routes(app); //register the route
+
 
 app.listen(port);
+
 
 console.log('todo list RESTful API server started on: ' + port);
